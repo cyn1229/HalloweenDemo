0dc438d10860857f69227a761a664bd8d071df89
cyn1229 <cyn_lyy@163.com>
2023-11-01 21:09:31 +0800

    万圣节第一节课

---
+++ Assets/BatFly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatFly : MonoBehaviour
+{
+    private Vector3 origPos;
+
+    private float elapse;
+
+    public float Interval = 0.05f;
+
+    public float Range = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
