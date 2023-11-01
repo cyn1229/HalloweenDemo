e1783fb55a335224ec1f5da2df52cdb03c1203ef
cyn1229 <cyn_lyy@163.com>
2023-11-01 22:33:00 +0800

    万圣节粒子效果

--- Assets/BatFly.cs
+++ Assets/BatFly.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class BatFly : MonoBehaviour
 {
-    private Vector3 origPos;
+    private Vector3 origPos;//储存位置
 
-    private float elapse;
+    private float elapse;//时间变量
 
     public float Interval = 0.05f;
 
@@ -18,8 +18,22 @@ public class BatFly : MonoBehaviour
     }
 
     // Update is called once per frame
+    /// <summary>
+    ///
+    /// 
+    /// </summary>
     void Update()
     {
-        
+        //经过了多少秒
+        elapse += Time.deltaTime;//deltaTime该变量显示了距上一帧所经历的时间值（以秒计算）
+        //每经过0.05f，当前位置加随机值，注意清空
+        if (elapse > Interval)//
+        {
+            var pos = transform.position;
+            pos.x = origPos.x + Random.Range(-Range, Range);
+            pos.y = origPos.y + Random.Range(-Range, Range);
+            transform.position = pos;
+            elapse = 0.0f;
+        }
     }
 }
